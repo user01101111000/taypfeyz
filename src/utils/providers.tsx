8019f5cd45e99469840d092778d30e9a07b56b95
sebuhiiiiii <sebuhiiiiii36@gmail.com>
2025-03-31 11:58:10 +0400

    fixing

--- src/utils/providers.tsx
+++ src/utils/providers.tsx
@@ -2,12 +2,20 @@ import React from "react";
 import { SettingsProvider } from "@/context/SettingsContext.tsx";
 import { Toaster } from "@/components/ui/shadcn/sonner.tsx"
 import { CodeProvider } from "@/context/CodeContext.tsx";
+import ReactGA from "react-ga4";
 
 type ProvidersProps = {
     children: React.ReactNode
 }
 
 const Providers: React.FC<ProvidersProps> = (props: ProvidersProps): React.JSX.Element => {
+
+    React.useEffect((): void => {
+        ReactGA.initialize(import.meta.env.VITE_GA_ID);
+
+        ReactGA.send({ hitType: "pageview", page: window.location.pathname, title: "Home" });
+    }, []);
+
     return <CodeProvider>
         <SettingsProvider>
             {props.children}
